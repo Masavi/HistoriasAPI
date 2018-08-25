1bff657df4b8a1aadff559c80919a9bbd711c1da
Masavi <masavi2008@hotmail.com>
2018-08-25 17:56:29 -0500

    agregar start script para montar en heroku

--- src/server.js
+++ src/server.js
@@ -10,12 +10,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parsear application/json
 app.use(bodyParser.json());
 
+// usar puerto 3000 a menos que exista un puerto preconfigurado
+app.set('port', process.env.PORT || 3000);
+
 // setear manejador de rutas en autores e historias
 app.use('/autores', autores);
 app.use('/historias', historias);
-
-// usar puerto 3000 a menos que exista un puerto preconfigurado
-app.set('port', process.env.PORT || 3000);
 
 // Página raíz del servidor
 app.get('/', (req, res) => {
